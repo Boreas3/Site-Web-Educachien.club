dfc38258647bb56cd5cc616b63a203115fadd8ed
Martin Heylen <martinmac@blocked.local>
2025-06-09 18:07:08 +0200

    rev1

---
+++ www/assets/scripts/photos/galleries.js
@@ -0,0 +1,12 @@
+const galleries = [
+    {title: 'Jeux sans frontières Challengers - 8 août 2023', src: "https://photos.app.goo.gl/VKRcwrsBcdEtRWmWA"},
+    {title: 'Test de sociabilité - 30 juillet 2023', src: "https://photos.app.goo.gl/Ke4VNeAMGE2XS8M56"},
+    {title: 'Anniversaire Aramis - 8 juillet 2023', src: "https://photos.app.goo.gl/HJfNjFiAT3jkQrEH8"},
+    {title: 'Cours', src: "https://photos.app.goo.gl/ZAhsNDBN1fZ1yn5o9"},
+    {title: 'Saint Nicolas - 2021', src: "https://photos.app.goo.gl/niZcTtNdaj8oqzC88"},
+    {title: 'Ballade éducative - 11 juillet 2021', src: "https://photos.app.goo.gl/5cfBs2NmPVy86VGCA"},
+    {title: 'Jeux & déguisements', src: "https://photos.app.goo.gl/dZtYQBeSJaRL5GnX8"},
+    {title: 'Anniversaire surprise Jean - 70 ans', src: "https://photos.app.goo.gl/6V61RyHp6aY4xQo8A"}
+];
+
+createGalleries();
